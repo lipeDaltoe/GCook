62cb92339680118465089e21212c7e1b545d86cd
lipeDaltoe <felipedaltoe9@gmail.com>
2024-08-22 12:29:30 -0300

    Models e Data

--- GCook/Models/ReceitaIngrediente.cs
+++ GCook/Models/ReceitaIngrediente.cs
@@ -20,7 +20,4 @@ public class ReceitaIngrediente
     [Required]
     [StringLength(30)]
     public string Quantidade { get; set; }
-
-    [StringLength(200)]
-    public string Preparo { get; set; }
 }
